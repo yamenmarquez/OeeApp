fcaffe4364cc9c37d6c9000647b57029ff5efb07
yamenmarquez <yamenmarquez@gmail.com>
2018-04-10 16:00:31 -0500

    Configuración inicial del menu principal de navegación
    Configuración inicial del menu principal de navegación

--- frontend/src/app/navigation/navigation.ts
+++ frontend/src/app/navigation/navigation.ts
@@ -1,11 +1,91 @@
 export const navigation = [
     {
         'id'      : 'applications',
-        'title'   : 'Applications',
+        'title'   : 'Aplicación',
         // Se desabilita la funcionalidad de traducción
         // 'translate': 'NAV.APPLICATIONS',
         'type'    : 'group',
         'children': [
+            {
+              'id'   : 'home',
+              'title': 'Inicio',
+              'type' : 'item',
+              'icon' : 'home',
+              'url'  : '#'
+            },
+            {
+              'id'        : 'planning' ,
+              'title'     : 'Planificación',
+              'type'      : 'collapse',
+              'icon'      : 'today',
+              'children'  : [
+                {
+                  'id'   : 'jobs',
+                  'title': 'Órdenes de producción',
+                  'type' : 'item',
+                  'icon' : 'create_new_folder',
+                  'url'  : '#'
+                }
+              ]
+            },
+            {
+              'id'        : 'operation' ,
+              'title'     : 'Operación',
+              'type'      : 'collapse',
+              'icon'      : 'business',
+              'children'  : [
+                {
+                  'id'   : 'startShift',
+                  'title': 'Iniciar turno',
+                  'type' : 'item',
+                  'icon' : 'arrow_forward',
+                  'url'  : '#'
+                },
+                {
+                  'id'   : 'openJobs',
+                  'title': 'Órdenes abiertas',
+                  'type' : 'item',
+                  'icon' : 'folder_open',
+                  'url'  : '#'
+                },
+                {
+                  'id'   : 'closeJobs',
+                  'title': 'Órdenes cerradas',
+                  'type' : 'item',
+                  'icon' : 'folder',
+                  'url'  : '#'
+                },
+                {
+                  'id'   : 'closeShift',
+                  'title': 'Cerrar turno',
+                  'type' : 'item',
+                  'icon' : 'arrow_back',
+                  'url'  : '#'
+                }
+              ]
+            },
+            {
+              'id'        : 'results' ,
+              'title'     : 'Resultados',
+              'type'      : 'collapse',
+              'icon'      : 'assessment',
+              'children'  : [
+                {
+                  'id'   : 'checkOeeResults',
+                  'title': 'Consultar Oee',
+                  'type' : 'item',
+                  'icon' : 'search',
+                  'url'  : '#'
+                },
+                {
+                  'id'   : 'checkHistoricalOee',
+                  'title': 'Oee histórico',
+                  'type' : 'item',
+                  'icon' : 'format_list_numbered',
+                  'url'  : '#'
+                }
+              ]
+            },
             {
                 'id'   : 'sample',
                 'title': 'Sample',
