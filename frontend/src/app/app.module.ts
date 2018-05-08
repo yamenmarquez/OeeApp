d38019a31417fe8c40ef1bec2ed38ab0cba67001
yamenmarquez <yamenmarquez@gmail.com>
2018-05-08 15:15:13 -0500

    Commit 16 ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,10 +16,24 @@ import { AppComponent } from './app.component';
 import { FuseMainModule } from './main/main.module';
 import { FuseSampleModule } from './main/content/sample/sample.module';
 
+// Servicios OeeApp
+
+import { AuthenticationService } from './main/content/pages/authentication/authentication.service';
+
+// Configuración de Apollo
+import {Apollo, ApolloModule} from 'apollo-angular';
+import {HttpLink, HttpLinkModule} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloLink} from 'apollo-link';
+
 const appRoutes: Routes = [
     {
+      path        : 'pages',
+      loadChildren: './main/content/pages/pages.module#FusePagesModule'
+    },
+    {
         path      : '**',
-        redirectTo: 'sample'
+        redirectTo: 'pages/auth/login'
     }
 ];
 
@@ -39,12 +53,30 @@ const appRoutes: Routes = [
         FuseModule.forRoot(fuseConfig),
         FuseSharedModule,
         FuseMainModule,
-        FuseSampleModule
+        FuseSampleModule,
+        // Apollo
+        ApolloModule,
+        HttpLinkModule
     ],
+    providers   : [
+      // Servicios OeeApp
+      AuthenticationService
+  ],
     bootstrap   : [
         AppComponent
     ]
 })
 export class AppModule
 {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+
+    const cache = new InMemoryCache(
+     // {dataIdFromObject: object => object.nodeId}
+    );
+
+    apollo.create({
+      link: httpLink.create({ uri: 'http://localhost:5000/graphql'}),
+      cache: cache
+    });
+  }
 }
